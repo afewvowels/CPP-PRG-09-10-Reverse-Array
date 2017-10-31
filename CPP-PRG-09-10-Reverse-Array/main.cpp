4069e91ecd960a12db3ec451a5313494538e9f07
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-31 18:16:58 -0400

    Prompt written and working.

--- CPP-PRG-09-10-Reverse-Array/main.cpp
+++ CPP-PRG-09-10-Reverse-Array/main.cpp
@@ -5,11 +5,18 @@
 //  Created by Keith Smith on 10/31/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a function that accepts an int array and the array's size as arguments. The
+//  function should create a copy of the array, except that the element values should be reversed
+//  in the copy. The function should return a pointer to the new array. Demonstrate the
+//  function in a complete program.
+
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    
     return 0;
 }
